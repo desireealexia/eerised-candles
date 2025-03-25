bb313cf34051769a620d8904764f89e84856f829
desireealexia <desireealexia19@gmail.com>
2025-03-25 17:25:13 +0000

    Cusomised admin

--- products/models.py
+++ products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Category(models.Model):
+        
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
