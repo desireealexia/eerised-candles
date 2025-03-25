86e102136663b54ef2ef8f5eba33ba6fdf254748
desireealexia <desireealexia19@gmail.com>
2025-03-25 17:07:39 +0000

    Added products app, models, fixtures (with placeholders)

---
+++ README.md
@@ -0,0 +1,29 @@
+# [EERISED Candles](#)
+
+EERISED Candles is an e-commerce platform that enables users to browse, purchase, and manage their orders for candles, reed diffusers and wax melts. The website offers an intuitive and visually appealing shopping experience with secure payment integration.
+
+![EERISED Candles Responsiveness](#)
+
+[View live website here](#)
+
+# User Experience (UX)
+
+## User Stories
+
+External Users:
+
+- As a user, I want to browse available products with detailed descriptions and images.
+- As a user, I want to create an account to track my purchases.
+- As a user, I want to add products to my shopping cart and update quantities.
+- As a user, I want to securely make payments through Stripe.
+- As a user, I want to view my past orders and receive order confirmations.
+- As a user, I want to save favourite products for future purchases.
+
+Site Owner:
+
+- As an admin, I want to manage product listings and inventory.
+- As an admin, I want to track and process customer orders.
+- As an admin, I want to ensure secure payment processing.
+
+# Design
+
---
+++ products/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='subcategories')
+    
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    stock = models.IntegerField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+    personalisation_details = models.TextField(null=True, blank=True) 
+
+    def __str__(self):
+        return self.name
