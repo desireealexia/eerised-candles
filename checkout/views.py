1c1832584e903497a728de8654c255a6c87dabbd
desireealexia <desireealexia19@gmail.com>
2025-04-01 19:45:45 +0100

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R8nRPR8jRiebaSJT8WvEQtzgb8xrwAfh3y9yUc4350bQPscbLgcKsSy0MmBINFo4PJRrOdjymj486IaZViABy3C0035DB2iku',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
